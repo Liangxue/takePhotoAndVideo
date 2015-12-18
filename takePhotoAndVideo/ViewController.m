5b87fe896740f19f439f34824a73bacd599bb992
Liangxue <xue@madeMacBook-Pro.local>
2015-12-18 09:49:04 +0800

    照相和录像
    调用系统自带的相机完成拍摄和录像，对于内容进行了本地存储

--- takePhotoAndVideo/ViewController.m
+++ takePhotoAndVideo/ViewController.m
@@ -1,27 +1,108 @@
 //
 //  ViewController.m
-//  takePhotoAndVideo
+//  拍照录像
 //
 //  Created by ma c on 15/12/15.
 //  Copyright © 2015年 梁学. All rights reserved.
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ViewController ()
+
+@interface ViewController ()<UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+
+@property (strong, nonatomic) UIImageView *photo;//照片展示视图
+@property (strong ,nonatomic) AVPlayer *player;//播放器，用于录制完视频后播放视频
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    UIImagePickerController *picker1;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.view.backgroundColor = [UIColor cyanColor];
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(100, 300, 100, 100);
+    btn.backgroundColor = [UIColor redColor];
+    [btn addTarget:self action:@selector(takeClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    self.photo = [UIImageView new];
+    self.photo.frame = CGRectMake(100, 100, 100, 100);
+    [self.view addSubview:self.photo];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UI事件
+//点击拍照按钮
+- (void)takeClick:(UIButton *)sender {
+    
+    NSArray* availableMediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
+    
+    picker1 = [[UIImagePickerController alloc]init];
+    picker1.sourceType=UIImagePickerControllerSourceTypeCamera;
+    picker1.mediaTypes = availableMediaTypes;
+    picker1.delegate = self;
+    [self presentViewController:picker1 animated:YES completion:nil];
+    picker1.allowsEditing = YES;
+    picker1.videoQuality = UIImagePickerControllerQualityTypeHigh;
+
 }
+
+#pragma mark - UIImagePickerController代理方法
+//完成
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    
+    NSString *mediaType=[info objectForKey:UIImagePickerControllerMediaType];
+    if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {//如果是拍照
+        UIImage *image;
+        //如果允许编辑则获得编辑后的照片，否则获取原始照片
+        if (picker1.allowsEditing) {
+            image=[info objectForKey:UIImagePickerControllerEditedImage];//获取编辑后的照片
+        }else{
+            image=[info objectForKey:UIImagePickerControllerOriginalImage];//获取原始照片
+        }
+        [self.photo setImage:image];//显示照片
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);//保存到相簿
+    }else if([mediaType isEqualToString:(NSString *)kUTTypeMovie]){//如果是录制视频
+        NSURL *url=[info objectForKey:UIImagePickerControllerMediaURL];//视频路径
+        NSString *urlStr=[url path];
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(urlStr)) {
+            //保存视频到相簿，注意也可以使用ALAssetsLibrary来保存
+            UISaveVideoAtPathToSavedPhotosAlbum(urlStr, self, @selector(video:didFinishSavingWithError:contextInfo:), nil);//保存视频到相簿
+        }
+        
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+//视频保存后的回调
+- (void)video:(NSString *)videoPath didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo{
+    if (error) {
+        NSLog(@"保存视频过程中发生错误，错误信息:%@",error.localizedDescription);
+    }else{
+        NSLog(@"视频保存成功.");
+        //录制完之后自动播放
+        NSURL *url=[NSURL fileURLWithPath:videoPath];
+        _player=[AVPlayer playerWithURL:url];
+        AVPlayerLayer *playerLayer=[AVPlayerLayer playerLayerWithPlayer:_player];
+        playerLayer.frame=self.photo.frame;
+        [self.photo.layer addSublayer:playerLayer];
+        [_player play];
+        
+    }
+}
+
 
 @end
